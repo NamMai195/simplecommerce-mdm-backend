696425586e15d017575c8bd4cd1b3de248f272ae
ThanhVinh05 <vinhptps38840@gmail.com>
2025-06-22 00:32:54 +0700

    feat: cấu hình exception

---
+++ src/main/java/com/simplecommerce_mdm/exception/ForBiddenException.java
@@ -0,0 +1,7 @@
+package com.simplecommerce_mdm.exception;
+
+public class ForBiddenException extends RuntimeException {
+    public ForBiddenException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/simplecommerce_mdm/exception/InvalidDataException.java
@@ -0,0 +1,8 @@
+package com.simplecommerce_mdm.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
+
