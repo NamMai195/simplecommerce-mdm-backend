4d9173b0786c40c240945588e89f452de6e60336
ThanhVinh05 <vinhptps38840@gmail.com>
2025-06-22 00:37:09 +0700

    bug: config swagger

--- src/main/java/com/simplecommerce_mdm/auth/controller/AuthController.java
+++ src/main/java/com/simplecommerce_mdm/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.simplecommerce_mdm.auth.controller;
 
-import com.simplecommerce_mdm.auth.dto.UserRegistrationDto;
+import com.simplecommerce_mdm.auth.dto.RegisterRequest;
 import com.simplecommerce_mdm.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Slf4j(topic = "AUTH-CONTROLLER")
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Register User", description = "API register new user to database")
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationDto registrationDto) {
-        authService.register(registrationDto);
+    public ResponseEntity<String> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
+        authService.register(registerRequest);
         return new ResponseEntity<>("User registered successfully!", HttpStatus.CREATED);
     }
 }
--- src/main/java/com/simplecommerce_mdm/auth/service/AuthService.java
+++ src/main/java/com/simplecommerce_mdm/auth/service/AuthService.java
@@ -1,8 +1,7 @@
 package com.simplecommerce_mdm.auth.service;
 
-
-import com.simplecommerce_mdm.auth.dto.UserRegistrationDto;
+import com.simplecommerce_mdm.auth.dto.RegisterRequest;
 
 public interface AuthService {
-    void register(UserRegistrationDto userRegistrationDto);
+    void register(RegisterRequest registerRequest);
 }
