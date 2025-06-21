65bb4c7b54befa599cfa317b3586c6d0b7a2270a
ThanhVinh05 <vinhptps38840@gmail.com>
2025-06-22 00:37:09 +0700

    bug: config swagger

--- src/main/java/com/simplecommerce_mdm/auth/dto/RegisterRequest.java
+++ src/main/java/com/simplecommerce_mdm/auth/dto/RegisterRequest.java
@@ -2,18 +2,23 @@ package com.simplecommerce_mdm.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UserRegistrationDto {
+public class RegisterRequest {
     @NotBlank(message = "First name is required")
     private String firstName;
 
     @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotBlank(message = "Phone is required")
+    @Pattern(regexp = "^\\d{10}$", message = "Phone number must be exactly 10 digits")
+    private String phoneNumber;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
